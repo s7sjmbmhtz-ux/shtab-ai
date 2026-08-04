2aa6a05f802387d4877b014b648c7db8294e9dfe
s7sjmbmhtz-ux <s7sjmbmhtz@privaterelay.appleid.com>
2026-08-04 16:05:35 +0500

    Add files via upload

---
+++ business_states.py
@@ -0,0 +1,18 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class BusinessToolStates(StatesGroup):
+    collecting = State()
+
+
+class MarketplaceStates(StatesGroup):
+    waiting_photo = State()
+    choosing_product_type = State()
+    waiting_product_name = State()
+    choosing_category = State()
+    waiting_features = State()
+    choosing_goal = State()
+    choosing_platform = State()
+    choosing_style = State()
+    confirming = State()
+    generating = State()
